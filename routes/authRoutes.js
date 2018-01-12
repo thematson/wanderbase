692d3152e5f645d09179eb22e1d0ec37d11f51d7
Eric Matson <matson@Erics-MacBook-Pro.local>
2018-01-12 16:27:16 -0500

    some changes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,23 +1,24 @@
 const passport = require('passport');
 
 module.exports = app => {
-
   app.get(
     '/auth/google',
     passport.authenticate('google', {
       scope: ['profile', 'email']
     })
-    );
+  );
 
   app.get(
     '/auth/google/callback',
-    passport.authenticate('google'), (req, res) => {
-      res.send('this is a callbak');
-    });
+    passport.authenticate('google'),
+    (req, res) => {
+      res.send('callback');
+    }
+  );
 
   app.get('/api/logout', (req, res) => {
     req.logout();
-    res.send(req.user);
+    res.redirect('/');
   });
 
   app.get('/api/current_user', (req, res) => {
